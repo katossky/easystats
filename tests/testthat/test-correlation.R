669306eadd0e3737372d3517d9c5e719d01f7566
Ousseynou SIMAL <ousseynou.simal@eleve.ensai.fr>
2026-03-26 12:36:04 +0100

    adjust test correlation

--- tests/testthat/test-correlation.R
+++ tests/testthat/test-correlation.R
@@ -1,4 +1,4 @@
 test_that("CI columns removed when ci = FALSE", {
-  x <- easytats::correlation(mtcars, ci = FALSE)
+  x <- correlation(mtcars, ci = FALSE)
   expect_false(any(c("CI", "CI_low", "CI_high") %in% colnames(x)))
 })
